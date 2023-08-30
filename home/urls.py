bede8fede64f2bf9acb3cfcb692cfdbe7e50d457
LuongLuyen <luongvanluyen2003@gmail.com>
2023-08-30 19:59:53 +0700

    page login

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get),
+    path('home/', views.getHome),
+    path('', views.getLogin),
 ]
   
--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Product
 
-def get(request):
+def getHome(request):
     product = Product.objects.all()
     return render(request, 'pages/home.html', {'product': product})
+
+def getLogin(request):
+    return render(request, 'pages/login.html')
